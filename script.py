3d240037fca1d3b153d7af4ce1556521bdcdf376
Raymond Ouyang <ouyangmingraymond@gmail.com>
2019-09-16 00:29:44 -0700

    Update script.py

--- script.py
+++ script.py
@@ -3,37 +3,40 @@ import copy
 class GameState:
     def __init__(self, board = None, turn = 'x'):
         if board == None:
-            self.board = {
+            self._board = {
                 'a': [None, None, None],
                 'b': [None, None, None],
                 'c': [None, None, None]
                 }
         else:
-            self.board = board
+            self._board = board
         self._col_order = ('a', 'b', 'c')
-        self._num_rows = len(self.board['a'])
-        self._num_cols = len(self.board)
+        self._num_rows = len(self._board['a'])
+        self._num_cols = len(self._board)
         self.computer = 'x'
         self.human = 'o'
         self.player_turn = turn
 
     def __str__(self):
-        string = ' ------\n'
+        string = ' -------\n'
         row = 2
         for _ in range(self._num_cols):
             string += str(row+1) + '|'
             for col in self._col_order:
-                if self.board[col][row] != None: string += self.board[col][row] + ' '
-                else                            : string += '  '
-            string += '\n'
+                if self._board[col][row] != None: string += self._board[col][row] + '|'
+                else                            : string += ' |'
+            string += '\n -------\n'
             row -=1
-        return string + ' ------\n  a b c'
+        return string + '  a b c'
 
     def copy(self):
-        return GameState(copy.deepcopy(self.board), self.player_turn)
+        return GameState(copy.deepcopy(self._board), self.player_turn)
 
     def make_move(self, player, move):
-        self.board[move[0]][int(move[1])-1] = player
+        if self._board[move[0]][int(move[1])-1] != None:
+            raise AssertionError
+        
+        self._board[move[0]][int(move[1])-1] = player
         if player == self.computer:
             self.player_turn = self.human
         else:
@@ -41,21 +44,21 @@ class GameState:
         return self
 
     def winner(self):
-        if self.board['a'][0] == self.board['a'][1] == self.board['a'][2] != None or \
-        self.board['a'][0] == self.board['b'][0] == self.board['c'][0] != None or    \
-        self.board['a'][0] == self.board['b'][1] == self.board['c'][2] != None:
-            return self.board['a'][0]
+        if self._board['a'][0] == self._board['a'][1] == self._board['a'][2] != None or \
+        self._board['a'][0] == self._board['b'][0] == self._board['c'][0] != None or    \
+        self._board['a'][0] == self._board['b'][1] == self._board['c'][2] != None:
+            return self._board['a'][0]
 
-        elif self.board['b'][0] == self.board['b'][1] == self.board['b'][2] != None or \
-        self.board['a'][1] == self.board['b'][1] == self.board['c'][1] != None:
-            return self.board['b'][1]
+        elif self._board['b'][0] == self._board['b'][1] == self._board['b'][2] != None or \
+        self._board['a'][1] == self._board['b'][1] == self._board['c'][1] != None:
+            return self._board['b'][1]
 
-        elif self.board['c'][0] == self.board['c'][1] == self.board['c'][2] != None or \
-        self.board['a'][2] == self.board['b'][1] == self.board['c'][0] != None:
-            return self.board['c'][0]
+        elif self._board['c'][0] == self._board['c'][1] == self._board['c'][2] != None or \
+        self._board['a'][2] == self._board['b'][1] == self._board['c'][0] != None:
+            return self._board['c'][0]
 
-        elif self.board['a'][2] == self.board['b'][2] == self.board['c'][2] != None:
-            return self.board['a'][2]
+        elif self._board['a'][2] == self._board['b'][2] == self._board['c'][2] != None:
+            return self._board['a'][2]
 
         elif len(self.all_possible_moves()) == 0:
             return 0
@@ -64,7 +67,7 @@ class GameState:
 
     def all_possible_moves(self):
         moves = set()
-        for col, row in self.board.items():
+        for col, row in self._board.items():
             for i in range(len(row)):
                 if row[i] == None:
                     moves.add('%s%d' % (col, i+1))
@@ -99,17 +102,37 @@ def minimax(game_state: GameState) -> str:
 
 
 if __name__ == "__main__":
-    a = GameState()
-    print("The computer is 'x'. You are 'o'. Please wait several seconds as the computer calculates its first move.....")
+    choice = input("Do you want to go first? (yes/no): ")
+    while choice not in ('yes', 'no'):
+        choice = input("Please enter in yes/no. Do you want to go first? (yes/no): ")
+    if choice == "yes":
+        a = GameState(turn='o')
+        print("The computer is 'x'. You are 'o'")
+        
+    else:
+        a = GameState()
+        print("The computer is 'x'. You are 'o'. Please wait several seconds as the computer calculates its first move.....")
+    
     while a.winner() == None:
-        move = minimax(a)[0]
-        a.make_move('x', move)
+        if a.player_turn == a.computer:
+            move = minimax(a)[0]
+            a.make_move('x', move)
+
+            
         print(a)
-        if a.winner() not in (0, None):
-            print(str(a.winner()) + ' has won')
+        if a.winner() != None:
             break
-        elif a.winner() == 0:
-            print("Tie")
-            break
+        
         user_move = input("It's your turn. Make a move, for example 'a1', or 'c3': ")
-        a.make_move('o', user_move)
+        while True:
+            try:
+                a.make_move('o', user_move)
+                break
+            except AssertionError:
+                user_move = input("That's illegal. It's your turn. Make a move, for example 'a1', or 'c3': ")
+                
+
+    if a.winner() not in (0, None):
+        print(str(a.winner()) + ' has won')
+    elif a.winner() == 0:
+        print("Tie")
